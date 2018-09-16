25301a53f2c6a21fe8e06ab9af0f60787c3d1bd7
Paulo Roberto <pauloroberto@natal.rn.gov.br>
2018-09-16 20:14:31 -0300

    Conclusão do backend

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://prmorais:Paulo13@ds119702.mlab.com:19702/curso-react-ingles"
+    'mongodb://prmorais:Paulo13@ds119702.mlab.com:19702/curso-react-ingles',
+  secreOrKey: 'secret'
 };
